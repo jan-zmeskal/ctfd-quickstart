4dca150d02c7e91a6f20d13b27518c44355e0ced
Jan Zmeskal <jzmeskal@email.cz>
2019-12-06 16:05:44 +0100

    Customizing distinct deployment

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/home/jan/Dropbox/red_hat/devconf_contest/oc new-app https://raw.githubusercontent.com/jan-zmeskal/ctfd-quickstart/master/templates/distinct-deployments.json
---
+++ destroy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/home/jan/Dropbox/red_hat/devconf_contest/oc delete all,pvc,sa --selector app=ctfd
